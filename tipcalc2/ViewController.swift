7e951c06e6ce1356d6afb19d23b3ed641dae2445
Maggie Gates <Maggie@maggie-gatess-macbook-pro.local>
2015-12-13 21:18:26 -0500

    Initial Commit

---
+++ tipcalc2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalc2
+//
+//  Created by Maggie Gates on 12/13/15.
+//  Copyright © 2015 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
